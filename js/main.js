6eb790d3aefe1706ba93dc5cc2c9c5e3f1c7f31f
ccalvop <carloscalvopareja@gmail.com>
2023-03-25 18:28:33 +0100

    3 modificacion main.js

--- js/main.js
+++ js/main.js
@@ -1,57 +1,69 @@
-let zoomFactor = 1;
-let offsetX = 0;
-let offsetY = 0;
-let randomOffsetX, randomOffsetY;
+const CANVAS_SIZE = 600;
+const MAX_ITERATIONS = 100;
+
+let mandelbrot = true;
+let c;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(CANVAS_SIZE, CANVAS_SIZE);
   pixelDensity(1);
-  randomOffsetX = random(-2, 2);
-  randomOffsetY = random(-2, 2);
+  c = createVector(random(-1, 1), random(-1, 1));
+  drawFractal();
 }
 
 function draw() {
-  drawMandelbrot();
-}
-
-function drawMandelbrot() {
-  loadPixels();
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      let a = map(x, 0, width, -2 / zoomFactor + offsetX + randomOffsetX, 2 / zoomFactor + offsetX + randomOffsetX);
-      let b = map(y, 0, height, -2 / zoomFactor + offsetY + randomOffsetY, 2 / zoomFactor + offsetY + randomOffsetY);
-
-      let ca = a;
-      let cb = b;
-      let n = 0;
-
-      while (n < 100) {
-        let aa = a * a - b * b;
-        let bb = 2 * a * b;
-        a = aa + ca;
-        b = bb + cb;
-        if (abs(a + b) > 16) {
-          break;
-        }
-        n++;
-      }
-
-      let brightness = map(n, 0, 100, 0, 255);
-      if (n === 100) {
-        brightness = 0;
-      }
-
-      let pix = (x + y * width) * 4;
-      pixels[pix + 0] = brightness;
-      pixels[pix + 1] = brightness;
-      pixels[pix + 2] = brightness;
-      pixels[pix + 3] = 255;
-    }
-  }
-  updatePixels();
+  // No need to update the canvas continuously
+  noLoop();
 }
 
 function mouseClicked() {
-  randomOffsetX = random(-2, 2);
-  randomOffsetY = random(-2, 2);
+  mandelbrot = !mandelbrot;
+  c = createVector(random(-1, 1), random(-1, 1));
+  drawFractal();
+}
+
+function drawFractal() {
+  loadPixels();
+
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let a = map(x, 0, width, -2, 2);
+      let b = map(y, 0, height, -2, 2);
+
+      let ca = a;
+      let cb = b;
+
+      if (!mandelbrot) {
+        ca = c.x;
+        cb = c.y;
+      }
+
+      let n = 0;
+
+      while (n < MAX_ITERATIONS) {
+        let aa = a * a - b * b;
+        let bb = 2 * a * b;
+
+        a = aa + ca;
+        b = bb + cb;
+
+        if (abs(a + b) > 16) {
+          break;
+        }
+
+        n++;
+      }
+
+      let brightness = map(n, 0, MAX_ITERATIONS, 0, 1);
+      brightness = map(sqrt(brightness), 0, 1, 0, 255);
+
+      let pixelIndex = (x + y * width) * 4;
+      pixels[pixelIndex + 0] = brightness;
+      pixels[pixelIndex + 1] = brightness;
+      pixels[pixelIndex + 2] = brightness;
+      pixels[pixelIndex + 3] = 255;
+    }
+  }
+
+  updatePixels();
 }
